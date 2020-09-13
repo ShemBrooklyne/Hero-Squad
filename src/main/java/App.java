240c49034c3f9f261666ee4e351a6345852643b7
ShemBrooklyne <shemnyatti@gmail.com>
2020-09-13 18:32:06 +0300

    created forms

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,13 @@
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
+import static spark.route.HttpMethod.post;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -23,15 +25,20 @@ public class App {
 
         get("/Hero-form",(req, res) ->{
             Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "hero-form.hbs");
+            return new ModelAndView(model, "Hero-form.hbs");
         }, new HandlebarsTemplateEngine());
 
 //        get("/Hero",(req, res) ->{
 //            Map<String, Object> model = new HashMap<>();
 //            ArrayList<Hero> Hero = Hero.getAllInstances();
 //            model.put("Hero",Hero);
-//            return new ModelAndView(model, "hero.hbs");
+//            return new ModelAndView(model, "Hero.hbs");
 //        }, new HandlebarsTemplateEngine());
+
+        get("/Hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "Hero.hbs");
+        }, new HandlebarsTemplateEngine());
 
 
 
@@ -42,13 +49,27 @@ public class App {
 
 //        get("/Squad",(req, res) ->{
 //            Map<String, Object> model = new HashMap<>();
-//            ArrayList<Squad> squads = Squad.getInstances();
+//            ArrayList<Squad> squad = Squad.getInstances();
 //            model.put("squad",Squad);
 //            ArrayList<Hero> members = Hero.getAllInstances();
 //            model.put("hero",members);
 //            model.put("allSquadMembers", newSquad.getSquadMembers());
 //            return new ModelAndView(model, "Squad.hbs");
 //        }, new HandlebarsTemplateEngine());
+
+//        post("/new/hero",(req, res) ->{
+//            Map<String, Object> model = new HashMap<>();
+//            String name = req.queryParams("name");
+//            Integer age = Integer.parseInt(req.queryParams("age"));
+//            String power = req.queryParams("power");
+//            String weakness = req.queryParams("weakness");
+//            Hero newHero = new Hero(name,age,power,weakness);
+//            req.session().attribute("item",name);
+//            model.put("item",req.session().attribute("item"));
+//            model.put("newHero",newHero);
+//            return new ModelAndView(model, "submit.hbs");
+//        }, new HandlebarsTemplateEngine());
+
 
 
 
